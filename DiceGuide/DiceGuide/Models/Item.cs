eeda27dd831be2bdbd9e48cbd280e00c417ac046
Wes Rupert <ora@outlook.com>
2015-09-10 23:34:39 -0700

    Add basic Item class and base helper class

--- DiceGuide/DiceGuide/Models/Item.cs
+++ DiceGuide/DiceGuide/Models/Item.cs
@@ -1,12 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DiceGuide.Models
 {
-    class Item
+    class Item : SerializableReference
     {
+
+        private string _name = string.Empty;
+        private string _type = string.Empty;
+        private uint? _weight = null;
+        private string _text = string.Empty;
+
+        /// <summary>
+        /// The item's name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// The item's type.
+        /// </summary>
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        /// <summary>
+        /// The item's weight.
+        /// </summary>
+        public uint? Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                _weight = value;
+                OnPropertyChanged("Weight");
+            }
+        }
+
+        /// <summary>
+        /// The item's description.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        /// <summary>
+        /// Creates an empty item.
+        /// </summary>
+        public Item() { }
+
+        /// <summary>
+        /// Creates a new item from import data.
+        /// </summary>
+        /// <param name="node"></param>
+        public Item(XmlNode node)
+        {
+            Debug.Assert(node.Name == "item", "Initializing item with wrong node type!");
+            Debug.Assert(node.HasChildNodes, "Initializing item with empty node!");
+            if (node.Name != "item" || !node.HasChildNodes)
+                return;
+
+            _name = GetStringFromNode(node, "name");
+            _type = GetStringFromNode(node, "type");
+            _weight = GetUnsignedFromNode(node, "weight");
+            _text = GetStringFromNode(node, "text", "\n");
+        }
+
+        public override bool WriteObject(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} item, {2} lbs.\n{3}", Name, Type, Weight.HasValue ? Weight.Value.ToString() : "?", Text);
+        }
     }
 }
---
+++ DiceGuide/DiceGuide/Models/SerializableReference.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DiceGuide.Models
+{
+    abstract class SerializableReference : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Writes the object to the XML writer.
+        /// </summary>
+        /// <param name="writer">The writer to pass the object into.</param>
+        public abstract void WriteToXML(XmlWriter writer);
+
+        /// <inheritdoc />
+        public abstract override string ToString();
+
+        /// <summary>
+        /// Gets the string value from the children of the given node.
+        /// </summary>
+        /// <param name="node">The node containing the desired information.</param>
+        /// <param name="target">The target information type.</param>
+        /// <param name="separator">The type of separator to distinguish multiple matches.</param>
+        /// <returns>The string value containing all matches of target in node, separated by the given separator.</returns>
+        protected static string GetStringFromNode(XmlNode node, string target, string separator = "")
+        {
+            return string.Join(
+                separator,
+                from XmlNode n in node.ChildNodes
+                    where n.Name == target
+                    select n.HasChildNodes ? n.FirstChild.Value : string.Empty);
+        }
+
+        /// <summary>
+        /// Gets the unsigned numeric value from the children of the given node.
+        /// </summary>
+        /// <param name="node">The node containing the desired information.</param>
+        /// <param name="target">The target information type.</param>
+        /// <returns>The string value containing the relevant match of target in node.</returns>
+        protected static uint? GetUnsignedFromNode(XmlNode node, string target)
+        {
+            bool matched = false;
+            uint total = 0;
+            foreach (string s in (from XmlNode n in node.ChildNodes where n.Name == "weight" select n.FirstChild.Value))
+            {
+                uint w;
+                if (uint.TryParse(s, out w))
+                {
+                    matched = true;
+                    total += w;
+                }
+            }
+            return matched ? (uint?)total : null;
+        }
+
+        /// <summary>
+        /// Gets the signed numeric value from the children of the given node.
+        /// </summary>
+        /// <param name="node">The node containing the desired information.</param>
+        /// <param name="target">The target information type.</param>
+        /// <returns>The string value containing the relevant match of target in node.</returns>
+        protected static int? GetSignedFromNode(XmlNode node, string target)
+        {
+            bool matched = false;
+            int total = 0;
+            foreach (string s in (from XmlNode n in node.ChildNodes where n.Name == "weight" select n.FirstChild.Value))
+            {
+                int w;
+                if (int.TryParse(s, out w))
+                {
+                    matched = true;
+                    total += w;
+                }
+            }
+            return matched ? (int?)total : null;
+        }
+
+        /// <summary>
+        /// Property changed event caller.
+        /// </summary>
+        /// <param name="propertyName">The name of the changed property.</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
